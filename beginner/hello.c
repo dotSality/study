2fdd6518594d9b4b827028f841dcd39d78b5d59d
dotSality <sality33@gmail.com>
2026-05-23 16:22:20 +0200

    repo init, started beginners section

---
+++ beginner/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("hello\n");
+    printf("Xa, Loh!\n");
+    return 0;
+}
---
+++ beginner/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 3;
+    char firstSymbol = 'e';
+    double float_number = 3.2;
+
+    int firstArray[] = {1, 29, -4, 13};
+
+    printf("");
+
+    return 0;
+}
